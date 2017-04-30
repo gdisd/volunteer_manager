1257afe38564d0b9476d70e2a5351a55099f6261
Lea Ann Bradford <ltrammer@gmail.com>
2017-04-30 10:12:43 -0700

    added user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is a thing' do
+    expect {User.new}.to_not raise_error
+  end
+
+  it 'has an email and password' do
+    user = User.new
+    user.email = 'bob@bob.com'
+    user.password = 'bobbob'
+    expect(user.save).to be true
+    user2 = User.find_by_email('bob@bob.com')
+    expect(user2.email).to eq 'bob@bob.com'
+  end
 end
