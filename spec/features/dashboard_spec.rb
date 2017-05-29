ffb2925fd403e0f9593807f16c6ee5bb61e09552
Lea Ann Bradford <ltrammer@gmail.com>
2017-05-28 20:34:10 -0700

    re-worked tests to account for admin accounts and statuses

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -6,15 +6,21 @@ RSpec.feature "Dashboard", type: :feature do
       Given 'I am on the sign up page' do
         # part of sign_up command
       end
-      Then 'I can sign up as user' do
-        sign_up('test@test.com', 'password')
-      end
       Then 'I need a class to register for' do
+        create_admin
+        sign_in('admin@admin.com', 'adminadmin')
         add_class('Beginning JavaScript', 'Qualcomm AY', '4', 'This is a beginning JavaScript Class. Bring a laptop and a learning attitude.','9:00 a.m.', '4:00 p.m.', 'http://www.meetup.com')
+        click_link 'Welcome, admin@admin.com'
+        sign_out
       end
+      Then 'I can sign up as user' do
+        create_approved_user('test@test.com', 'password')
+        sign_in('test@test.com', 'password')
+      end
+
       Then 'I can register for an event' do
       # add a class to be able to register
-        visit '/'
+        visit '/dashboard'
         click_link 'Beginning JavaScript'
         click_link 'TA for this class'
         expect(page).to have_content('You are signed up to TA for Beginning JavaScript')
--- spec/features/events_spec.rb
+++ spec/features/events_spec.rb
@@ -7,17 +7,18 @@ RSpec.feature "Events", type: :feature do
         # part of sign_up command
       end
       Then 'I can sign up as admin' do
-        sign_up('admin@gdi.com', 'password')
+        create_admin
+        sign_in('admin@admin.com', 'adminadmin')
       end
       Then 'I can create an event' do
-      add_class('Beginning JavaScript', 'Qualcomm AY', '4', 'This is a beginning JavaScript Class. Bring a laptop and a learning attitude.','9:00 a.m.', '4:00 p.m.', 'http://www.meetup.com')
-      expect(page).to have_content('Beginning JavaScript')
-      expect(page).to have_content('Qualcomm AY')
-      expect(page).to have_content('4')
-      expect(page).to have_content('This is a beginning JavaScript Class. Bring a laptop and a learning attitude.')
-      expect(page).to have_content('9:00 a.m.')
-      expect(page).to have_content('4:00 p.m.')
-      expect(page).to have_content('http://www.meetup.com')
+        add_class('Beginning JavaScript', 'Qualcomm AY', '4', 'This is a beginning JavaScript Class. Bring a laptop and a learning attitude.','9:00 a.m.', '4:00 p.m.', 'http://www.meetup.com')
+        expect(page).to have_content('Beginning JavaScript')
+        expect(page).to have_content('Qualcomm AY')
+        expect(page).to have_content('4')
+        expect(page).to have_content('This is a beginning JavaScript Class. Bring a laptop and a learning attitude.')
+        expect(page).to have_content('9:00 a.m.')
+        expect(page).to have_content('4:00 p.m.')
+        expect(page).to have_content('http://www.meetup.com')
       end
     end
   end
