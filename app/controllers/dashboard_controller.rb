9a23f5f17d1c7239c08ef9545e55165809e9852f
Lea Ann Bradford <ltrammer@gmail.com>
2017-05-28 18:30:45 -0700

    after signin go to dashboard, update route for about page, if you aren't logged in you can't get to the dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,15 @@
 class DashboardController < ApplicationController
 
   def index
-    @user = current_user
-    @application = Application.new
-    @events = Event.all
+    # nobody logged in & user is trying to go to /profile (hacking)
+    if current_user.nil?
+      redirect_to '/users/sign_up'
+    # if you are logged in
+    else
+      @user = current_user
+      @application = Application.new
+      @events = Event.all
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations', passwords: 'users/passwords', sessions: 'users/sessions' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'about/index'
+  get '/about' => 'about#index'
   get '/dashboard' => 'dashboard#index'
   get 'events/:id/add_ta' => 'events#add_ta'
   get 'events/:id/remove_ta' => 'events#remove_ta'
