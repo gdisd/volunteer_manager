10e231469275ef46207cad8e8e5abe04fa6e38d7
Lea Ann Bradford <ltrammer@gmail.com>
2017-05-28 20:03:11 -0700

    added get_involved page, more changes to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   get '/dashboard' => 'dashboard#index'
   get 'events/:id/add_ta' => 'events#add_ta'
   get 'events/:id/remove_ta' => 'events#remove_ta'
+  get 'get_involved' => 'welcome#get_involved'
   root 'welcome#index'
 end
