ac918c34ee8cdfe1497b7504af6fa5d738b7f1a1
Lea Ann Bradford <ltrammer@gmail.com>
2017-06-03 23:32:34 -0700

    admin dashboard now has list of users, pending users can be updated to approved, added bootstrap classes for forms and buttons

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,10 +6,16 @@ class DashboardController < ApplicationController
       redirect_to '/users/sign_up'
     # if you are logged in
     else
+      @users = User.all
       @user = current_user
       @application = Application.new
       @events = Event.all
     end
   end
 
+  def set_user_status_to_approved
+    @user = User.find(params[:user_id])
+    @user.set_approved
+    redirect_to '/dashboard', notice: "#{@user.email} is now approved for volunteering as a TA"
+  end
 end
