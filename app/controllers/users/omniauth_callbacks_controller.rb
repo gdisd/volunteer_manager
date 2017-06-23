58f5f3527bdca0e3b258603fb593d1a17fb3f3f0
gabriel <ggiestas@gmail.com>
2017-06-23 13:57:42 -0700

    meetup login is working

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,7 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def meetup
     # You need to implement the method below in your model (e.g. app/models/user.rb)
+    # debugger
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
@@ -8,6 +9,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       set_flash_message(:notice, :success, :kind => "Meetup") if is_navigational_format?
     else
       session["devise.meetup_data"] = request.env["omniauth.auth"]
+      puts @user.errors
       redirect_to new_user_registration_url
     end
   end
